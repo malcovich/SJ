d487aae4de75f7d5f82597315ad4a8b1fd99543e
Dmytro Vertii <dmytro.vertii@tx.local>
2017-07-12 19:51:17 +0300

    update

--- www/js/main/main.controller.js
+++ www/js/main/main.controller.js
@@ -1,18 +1,27 @@
 angular.module('starter')
   .controller('MainController', ['$scope','AuthFactory','$http','$stateParams','$ionicModal','baseUrl', function($scope, AuthFactory, $http, $stateParams, $ionicModal, baseUrl){
     var $ctrl = this;
+    $ctrl.baseUrl = baseUrl;
     AuthFactory.me().then(function(res){
       $ctrl.user = res.data.data;
 		$http.post(baseUrl + '/api/friend/listFriendsRequests',{userId: $ctrl.user._id}).then(function(res,err){
 			$ctrl.listRequest = res.data;
-			console.log('!', res.data)
-
 		})
 
 		$ctrl.accept = function(id){
 			$http.post(baseUrl + '/api/friend/accept',{'_id': id }).then(function(res){
 				console.log(res)
 			});
+		};
+
+		$ctrl.search = function(){
+			if ($ctrl.searchStr.length >=3 ){
+				$http.post(baseUrl + '/api/search',{'q': $ctrl.searchStr}).then(function(res){
+					$ctrl.searchResut = res.data;
+				});
+			}else {
+				$ctrl.searchResut = [];
+			}
 		}
     });
 }]);
